1fb852ebbe6a4271072ced9186cd0412e5ee5323
Embury1 <patrik.sundstrom@gmail.com>
2017-12-19 20:54:47 +0100

    log complete object if provided

--- src/api/log.js
+++ src/api/log.js
@@ -1,14 +1,17 @@
 import moment from 'moment-timezone';
+import { inspect } from 'util';
 
 export default {
     info,
     error
 };
 
-function info(msg) {
+function info(msg, obj) {
     console.log(`[${moment().format('HH:mm:ss YYYY-MM-DD')}] ${msg}`);
+    if (obj) console.log(inspect(obj, false, null));
 }   
 
-function error(msg) {
+function error(msg, obj) {
     console.error(`[${moment().format('HH:mm:ss YYYY-MM-DD')}] ${msg}`);
+    if (obj) console.log(inspect(obj, false, null));
 }   
