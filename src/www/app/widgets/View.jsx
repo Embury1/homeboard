aacbc23e9956520b06b91e8d028ce496d924a7f6
Embury1 <patrik.sundstrom@gmail.com>
2018-02-15 21:11:14 +0100

    Create widget carousel

--- src/www/app/widgets/View.jsx
+++ src/www/app/widgets/View.jsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import FontAwesome from 'font-awesome/css/font-awesome.css';
+
 import * as Widgets from './Widgets';
+import styles from './View.css';
 
-const toView = (ref) => {
-    const [widget, mode] = ref.split('/');
-    return Widgets[widget][mode];
-};
-
-export default function(props) {
-    const views = props.refs.map((ref) => {
-	const View = toView(ref);
-	return <View key={ref} {...props} />;
-    });
-
-    if (!views.length) {
-	const ref = 'Placeholder/View';
-	const Placeholder = toView(ref);
-	views.push(<Placeholder key={ref} />);
+class View extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    refIndex: 0
+	};
     }
 
-    return (
-	<div>{views}</div>
-    );
+    componentWillReceiveProps(nextProps) {
+	const refIndex = this.state.refIndex;
+	if (refIndex > 0 && refIndex >= (nextProps.refs.length - 1)) {
+	    this.setState({ refIndex: nextProps.refs.length - 1 });
+	}
+    }
+
+    toView = (ref) => {
+	const [widget, mode] = ref.split('/');
+	return Widgets[widget][mode];
+    };
+
+    previous = (event) => {
+	this.setState((prevState) => ({ refIndex: prevState.refIndex - 1 }));
+    };
+
+    next = (event) => {
+	this.setState((prevState) => ({ refIndex: prevState.refIndex + 1 }));
+    };
+
+    render() {
+	const refIndex = this.state.refIndex;
+
+	const ref = this.props.refs.length
+	    ? this.props.refs[this.state.refIndex]
+	    : 'Placeholder/View';
+
+	const Widget = this.toView(ref);
+
+	return (
+	    <React.Fragment>
+		{this.props.refs.length > 1 && this.state.refIndex > 0 &&
+		    <div className={styles.previous} onClick={this.previous}>
+			<i className={[FontAwesome.fa, FontAwesome['fa-angle-left']].join(' ')}></i> 
+		    </div>
+		}
+
+		<Widget {...this.props} />
+
+		{this.props.refs.length > 1 && this.state.refIndex < (this.props.refs.length - 1) &&
+		    <div className={styles.next} onClick={this.next}>
+			<i className={[FontAwesome.fa, FontAwesome['fa-angle-right']].join(' ')}></i> 
+		    </div>
+		}
+	    </React.Fragment>
+	);
+    }
 }
+
+export default View;
