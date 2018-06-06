a7ec9ab0c0c62545106246952b2e9aeed653428f
Embury1 <patrik.sundstrom@gmail.com>
2018-06-06 16:11:01 +0200

    Create notFound component

---
+++ src/www/app/widgets/notFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render () {
+        return <h2>Component has gone missing</h2>;
+    }
+}
+
+export { NotFound };
